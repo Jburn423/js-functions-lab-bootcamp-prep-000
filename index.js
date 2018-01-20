b28c34b255311953a2c1688fdb197eb91dd82d33
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-20 22:12:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ function happyHolidays(){
 }happyHolidays();
 
 // Part #2
+// function happyHolidaysTo(name){
+//   return('Happy holidays, you!');
+// }happyHolidaysTo();
+
 function happyHolidaysTo(name){
-  return('Happy holidays, you!');
-  //console.log('Happy holidays, ${name}!');
-}happyHolidaysTo();
+  return('Happy holidays, ${name}!');
+}happyHolidaysTo('you');
+
+but it did not work
 
 // Part #3
 // function happyHolidaysTo(holiday, name){
